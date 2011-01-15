55007ec2b681d4a4ac63e99406f969fd05edbbc8
Sebastian Kosch <cone@aldusleaf.org>
2011-01-15 16:08:22 -0500

    Got LCD to work.

--- silence/main.asm
+++ silence/main.asm
@@ -4,7 +4,7 @@ list P=18F4620, F=INHX32, C=160, N=80, ST=OFF, MM=OFF, R=DEC
 #include <macros.inc>
 #include <lcd18.inc>
 
-change made in lcd-test.
+
 ;;;;;;Configuration Bits;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 		CONFIG OSC=HS, FCMEN=OFF, IESO=OFF
@@ -49,8 +49,7 @@ tmp1	res 1			;temporary storage for calculations
 
 IHR
 	;; if it was a key input, go to the menu handling thing.
-	btfsc	PORTB,1		;
-	goto	menu_handler
+	
 	
 	
 	
@@ -81,143 +80,12 @@ movlw	b'10101010'
 	call	menu_drawscreen
 	goto	Mainline
 	
-	;; ****************************************
-	;; KPHexToInt converts key_pressed into the corresponding number
-	;; ****************************************
-KPHexToInt
-	movlw	upper KeyTbl
-	movwf	TBLPTRU
-	movlw	high KeyTbl
-	movwf	TBLPTRH
-	movlw	low KeyTbl
-	addwf	key_pressed, W	;add the key offset to the lower end of the table
-	movwf	TBLPTRL
-	movlw	0
-	addwfc	TBLPTRH, F	;let the carry ripple up if we went too far
-	addwfc	TBLPTRU, F
 
-	tblrd	*		;read that thing into the latch
-	
-	movf	TABLAT, W	;put it into W
-	movwf	key_pressed
-	
-KeyTbl	db 1, 2, 3, 0, 4, 5, 6, 0, 7, 8, 9, 0, 0, 0, 0, 0 ;the list of values
-	return
-
-
-	;; ****************************************
-	;; Menu Handler: Takes care of navigation through the menu
-	;; ****************************************
-menu_handler
-	swapf	PORTB,W		;read PortB<7:4> into W<3:0>
-	andlw	0x0F		;mask the lower nibble
-	rlncf	WREG, W		;program memory counts up by two
-	movwf	key_pressed
-
-	select state
-	case 0			;Welcome screen
-		movf	key_pressed, w
-		xorlw	15	;D:Next
-		gtifz	menu_next
-
-	endcase
-	case 1			;Load Prompt
-		movf	key_pressed, w
-		xorlw	12	;*:Back
-		gtifz	menu_back	
-		xorlw	3 ^ 12	;A:Load
-		gtifz	menu_next
-		xorlw	15 ^ 12	;D:Next
-		gtifz	menu_skip
-
-	endcase
-	case 2			;Offset Prompt
-		PointFSR0To	offset_bcd ;append to offset variable
-		movf	key_pressed, w
-		xorlw	12	;*:Back
-		gtifz	menu_back	
-		xorlw	11 ^ 12	;C:Delete
-		gtifz	parameter_delete
-		xorlw	15 ^ 11	;D:Next
-		gtifz	menu_skip	
-
-	;; test for other keys AB#:
-		xorlw	3 ^ 15
-		gtifz	Mainline
-		xorlw	7 ^ 3
-		gtifz	Mainline
-		xorlw	14 ^ 7
-		gtifz	Mainline
-
-	;; otherwise, it's a number key. append.
-		goto	parameter_append
-	
-	endcase
-	case 3			;Interval Prompt
-
-	endcase
-	endselect
-	
-	movf	PORTB, f
-
-	goto	Mainline
-
-
-
-	;; ****************************************
-	;; Adds an offset or interval character and updates the display
-	;; ****************************************
-parameter_append
-	call	KPHexToInt
-	;; rotate left by 4 bits
-	rlcf	INDF0, f, 4
-	andlw	b'00001111'
-	andwf	INDF0
-	movwf	INDF0
-	goto 	Mainline
-	return
-
-	;; ****************************************
-	;; Deletes an offset or interval character and updates the display
-	;; ****************************************
-parameter_delete
-	rrncf	INDF0, f, 4
-	movlw	8
-	rrcf	PLUSW0,f, 4	;goto the upper byte and shift it down by four as well
-	goto Mainline		;just in case.
-
-
-
-	;; ****************************************
-	;; Increases the state counter and draws the screen
-	;; ****************************************
-menu_next
-	incf	state
-	goto	menu_drawscreen
-
-	;; ****************************************
-	;; Decreases the state counter and draws the screen
-	;; ****************************************
-menu_back
-	decf	state
-	goto	menu_drawscreen
-
-	;; ****************************************
-	;; Increases the state counter by two and draws the screen
-	;; ****************************************
-menu_skip
-	incf	state
-	incf	state
-	goto	menu_drawscreen
-
-	;; ****************************************
-	;; Draws on the display whatever the current state calls for.
-	;; ****************************************
 menu_drawscreen
 	;;  first, load the tablepointer to the tables and write the first and
 	;;  second line to the display.
 	;;  then, take care of inserting the current variable values.
-	call	ClrLCD
+	
 	movlw	upper LCDTbl_1
 	movwf	TBLPTRU
 	movlw	high LCDTbl_1
@@ -226,7 +94,7 @@ menu_drawscreen
 	movff	state, tmp1
 	movlw	16
 	mulwf	tmp1		;now tmp1 = state * 16
-	movwf	tmp1
+	movf	tmp1,w 
 
 	movlw	low LCDTbl_1
 	addwf	tmp1, W		;add the key offset*16 to the lower end of the table
@@ -237,27 +105,36 @@ menu_drawscreen
 	addwfc	TBLPTRU, F
 	tblrd	*		;read that thing into the latch
 	movf	TABLAT, W	;put it into W
+	movwf	PORTC
 NextChar_1
 	call	WrtLCD		;loop through the table entry until 0 is hit
 	tblrd	+*
 	movf	TABLAT,W
 	bnz	NextChar_1
 
-	;; write second line
-	movlw	.20		;go to position 20
-	call	PosLCD		;MIGHT NOT WORK correctly -- experimental
+	movlw	0xC0		;position cursor at second line. 0x80 + 0x40 = 0xC0
+	call	CmdLCD
+	
 	movlw	upper LCDTbl_2
 	movwf	TBLPTRU
 	movlw	high LCDTbl_2
 	movwf	TBLPTRH
+	
+	movff	state, tmp1
+	movlw	16
+	mulwf	tmp1		;now tmp1 = state * 16
+	movf	tmp1,w 
+
 	movlw	low LCDTbl_2
-	addwf	state, W	;add the key offset to the lower end of the table
+	addwf	tmp1, W		;add the key offset*16 to the lower end of the table
+	
 	movwf	TBLPTRL
 	movlw	0
 	addwfc	TBLPTRH, F	;let the carry ripple up if we went too far
 	addwfc	TBLPTRU, F
 	tblrd	*		;read that thing into the latch
 	movf	TABLAT, W	;put it into W
+	
 NextChar_2
 	call	WrtLCD		;loop through the table entry until 0 is hit
 	tblrd	+*
@@ -265,30 +142,31 @@ NextChar_2
 	bnz	NextChar_2	
 
 	
-	
+	goto 	Mainline
 	;; Congrats! By now, the standard content for each state should be written.
 	
 	select state
 	case 	3		;add the offset value
 	movlw 	.7
-	call	PosLCD
+	call	CmdLCD
 	;; write offset
 	endcase
 	case 	4			;add the interval value
 	movlw	.9
-	call	PosLCD
+	call	CmdLCD
 	;; write interval
 	endcase
 	endselect
 
+	bcf	INTCON3, INT1IF	;clear RB1 interrupt bit	
+	goto Mainline
 LCDTbl_1
-	db "Welcome        ",0,"         Load:A",0,"Ins Cone!   Up:A",0,"Offset:_       ",0,"Interval:_     ",0,"Ready!         ",0,"Deploying ...  ",0,"Success! Stats:A",0,"   cm( ) Next:A",0,"Store?    Yes:A",0
+	db "Welcome         ",0,"         Load:A",0,"Ins Cone!   Up:A",0,"Offset:_       ",0,"Interval:_     ",0,"Ready!         ",0,"Deploying ...  ",0,"Success! Stats:A",0,"   cm( ) Next:A",0,"Store?    Yes:A",0
 LCDTbl_2
 	db "         Start:D",0,"*:Back    Next:D",0,"*:Back    Next:D",0,"*:Back    Next:D",0,"*:Back    Next:D",0,"         Abort:C",0,"*:Back          ",0,"*:Back TDist:   ",0,"*:Back      No:B",0
 
 	;; Congrats again! The display has been written! Go back and chill!
 
-	goto Mainline
 	
 
 
@@ -298,6 +176,5 @@ LCDTbl_2
 
 	
 Mainline
-	bcf	INTCON3, INT1IF	;clear RB1 interrupt bit	
 	goto	$
 end
